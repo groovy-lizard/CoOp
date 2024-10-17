ed85e3e3da7126ed4f02ca1a32994ca96b68b542
Lucas Mayer <lmceschini@inf.ufrgs.br>
2024-10-17 09:58:19 -0300

    improving fine-tune script

--- fine-tune-fairface.sh
+++ fine-tune-fairface.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-bash scripts/coop/main.sh fairface rn50_ep50 end 8 1 False
+# DATASET: Name of dataset from Dassl configs
+# CFG: Config file stating the backbone model
+# CTP: Class Token Position (end or middle)
+# NCTX: Number of context tokens
+# SHOTS: Number of shots (1, 2, 4, 8, 16)
+# CSC: Class-specific context (False or True)
+
+# bash scripts/coop/main.sh <DATASET> <CFG> <CTP> <NCTX> <SHOTS> <CSC>
+
+bash scripts/coop/main.sh fairface rn50_ep50 middle 16 1 False
