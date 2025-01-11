17f513e3a1d0a301b34dbe0774a1619e541fb61e
Lucas Mayer <lucas.ceschini.prestserv@petrobras.com.br>
2025-01-11 18:19:37 -0300

    asd

--- fine-tune-fairface.sh
+++ fine-tune-fairface.sh
@@ -9,4 +9,4 @@
 
 # bash scripts/coop/main.sh <DATASET> <CFG> <CTP> <NCTX> <SHOTS> <CSC>
 
-bash scripts/coop/main.sh fairface vit_b32_ep50 end 16 16 False
+bash scripts/coop/main.sh fairface vit_b32_ep50 end 1 16 False
